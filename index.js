af85b8deddc431cee73d020fb734d952820ae725
Veronica Miller <vmcmiller@gmail.com>
2019-03-10 19:45:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function setThisWithCall(fn, thisValue, arg) {
   return fn.call(thisValue, arg);
 };
 
-function setThisWithApply(fn, thisValue, arg) {
-  return fn.apply(thisValue, [args]);
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, args);
 };
